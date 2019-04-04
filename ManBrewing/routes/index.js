ed84944cc590bd5ed33b49578d9bf0e434e7adf8
Nathan Dykstra <dykstra.nathan@gmail.com>
2019-04-03 22:37:08 -0500

    initial commit

---
+++ ManBrewing/routes/index.js
@@ -0,0 +1,39 @@
+﻿'use strict';
+var express = require('express');
+var mysql = require('mysql');
+var router = express.Router();
+
+var con = mysql.createConnection({
+    host: "sulaco",
+    user: "service",
+    password: "service",
+    database: "BeerRoom"
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    var countQuery = 'SELECT COUNT(*) AS rows FROM DataLog';
+    con.query(countQuery, function (err, result) {
+        if (err) throw err;
+        res.render('index', { title: 'M.A.N. Brewing', recordCount: result[0].rows });
+    });
+});
+
+router.post('/beerroom/environment', (req, res) => {
+    res.send("received");
+
+    console.log("temp: " + req.body.temp + ", humidity: " + req.body.humidity);
+
+    var insertQuery = 'INSERT INTO DataLog (temperature, humidity) VALUES (' + mysql.escape(req.body.temp) + ', ' + mysql.escape(req.body.humidity) + ')';
+    con.query(insertQuery, function (err, result) {
+        if (err) throw err;
+        console.log("Result: " + result.insertId);
+    });
+});
+
+module.exports = router;
