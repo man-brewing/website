70cc0934d5cbed172dd88b1b152b2d37746d59b9
Nathan Dykstra <dykstra.nathan@gmail.com>
2019-04-09 21:28:48 -0500

    working chart for temp and humidity data

--- ManBrewing/routes/index.js
+++ ManBrewing/routes/index.js
@@ -20,23 +20,42 @@ router.get('/', function (req, res, next) {
     res.redirect('/beerroom/environment');
 });
 
-router.get('/beerroom/environment', (req, res) => {
-    var countQuery = 'SELECT COUNT(*) AS rows FROM DataLog';
-    con.query(countQuery, function (err, result) {
-        if (err) throw err;
+router.get('/beerroom', function (req, res, next) {
+    res.redirect('/beerroom/environment');
+});
 
-        var recordCount = result[0].rows;
+router.get('/beerroom/environment', (req, res) => {
+    var chartLabels = [];
+    var temperatureData = [];
+    var humidityData = [];
+
+    var countQuery = 'SELECT COUNT(*) AS rows FROM DataLog';
+    con.query(countQuery, function (err, countResult) {
+        if (err) throw err;
 
         var currentQuery = 'SELECT dl.temperature, dl.humidity, dl.timestamp FROM DataLog dl ORDER BY dl.timestamp DESC LIMIT 50';
         con.query(currentQuery, function (err, result) {
             if (err) throw err;
 
-            var celsius = result[0].temperature.toFixed(2);
-            var fahrenheit = (result[0].temperature * 9 / 5 + 32).toFixed(2);
-            var humidity = result[0].humidity.toFixed(2);
-            var timestamp = result[0].timestamp;
+            for (var i = 0; i < result.length; i++) {
+                temperatureData.push((result[i].temperature * 9 / 5 + 32).toFixed(2));
+                humidityData.push(result[i].humidity.toFixed(2));
+                chartLabels.push(result[i].timestamp);
+            }
 
-            res.render('index', { title: 'M.A.N. Brewing', recordCount: recordCount, celsius: celsius, fahrenheit: fahrenheit, humidity: humidity, timestamp: timestamp });
+            var pugData = {
+                title: 'M.A.N. Brewing',
+                recordCount: countResult[0].rows,
+                celsius: result[0].temperature.toFixed(2),
+                fahrenheit: (result[0].temperature * 9 / 5 + 32).toFixed(2),
+                humidity: result[0].humidity.toFixed(2),
+                timestamp: result[0].timestamp,
+                tempData: JSON.stringify(temperatureData),
+                humidityData: JSON.stringify(humidityData),
+                chartLabels: JSON.stringify(chartLabels)
+            };
+
+            res.render('index', pugData);
         });
     });
 });
@@ -44,12 +63,9 @@ router.get('/beerroom/environment', (req, res) => {
 router.post('/beerroom/environment', (req, res) => {
     res.send("received");
 
-    console.log("temp: " + req.body.temp + ", humidity: " + req.body.humidity);
-
     var insertQuery = 'INSERT INTO DataLog (temperature, humidity) VALUES (' + mysql.escape(req.body.temp) + ', ' + mysql.escape(req.body.humidity) + ')';
     con.query(insertQuery, function (err, result) {
         if (err) throw err;
-        console.log("Result: " + result.insertId);
     });
 });
 
