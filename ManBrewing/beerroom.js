6a7a3766915e9b6fe42da1caa53dcd8ff9645c75
Nathan Dykstra <dykstra.nathan@gmail.com>
2019-04-14 21:39:44 -0500

    moving js and css files for express to serve the properly

--- ManBrewing/beerroom.js
+++ ManBrewing/beerroom.js
@@ -22,7 +22,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use("/images", express.static(path.join(__dirname, 'public/images')));
+app.use("/javascripts", express.static(path.join(__dirname, 'public/javascripts')));
+app.use("/stylesheets", express.static(path.join(__dirname, 'public/stylesheets')));
 
 app.locals.moment = require('moment');
 
