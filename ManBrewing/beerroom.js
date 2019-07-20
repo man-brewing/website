593fc0a0aaed3b0e7009a8ea4a0fc627f75c998a
Nathan Dykstra <dykstra.nathan@gmail.com>
2019-07-19 23:32:38 -0500

    added winston logging middleware and basic log messages

--- ManBrewing/beerroom.js
+++ ManBrewing/beerroom.js
@@ -5,10 +5,9 @@ var debug = require('debug');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var logger = require('./lib/log.js');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -18,9 +17,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// port config
+app.set('port', process.env.PORT || 3000);
+
 // uncomment after placing your favicon in /public
 //app.use(favicon('/beerroom/public/images/favicon.ico'));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -31,19 +32,27 @@ app.locals.moment = require('moment');
 app.use('/', routes);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
+/**
+ * Catch 404 and forward to error handler
+ */
 app.use(function (req, res, next) {
+    logger.error('404 Not Found');
+    logger.error(req);
+
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
+/**
+ *
+ * Development error handler, will print stacktrace
+ */
+if (process.env.ENV === 'DEVELOPMENT') {
     app.use(function (err, req, res, next) {
+        logger.fatal(err);
+        logger.fatal(req);
+
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -52,9 +61,13 @@ if (app.get('env') === 'development') {
     });
 }
 
-// production error handler
-// no stacktraces leaked to user
+/**
+ * Production error handler, no stacktraces leaked to user
+ */
 app.use(function (err, req, res, next) {
+    logger.fatal(err);
+    logger.fatal(req);
+
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
@@ -62,8 +75,9 @@ app.use(function (err, req, res, next) {
     });
 });
 
-app.set('port', process.env.PORT || 3000);
-
+/**
+ * Starts the server on the configure port and starts listening for requests.
+ */
 var server = app.listen(app.get('port'), function () {
-    debug('Express server listening on port ' + server.address().port);
+    logger.info('Express server listening on port ' + server.address().port);
 });
