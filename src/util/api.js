e86b2fdda3dbee33d42490fd14b086ec5cc79c4b
Nathan Dykstra <NathanDykstra@users.noreply.github.com>
2020-02-24 21:50:33 -0600

    handle api errors

--- src/util/api.js
+++ src/util/api.js
@@ -1,3 +1,8 @@
 export default function GetEnvironmentData (count = 20) {
+    // for testing broken api
+    // return new Promise((resolve) => {
+    //     setTimeout(resolve, 1000);
+    // });
+
     return fetch('http://krimthered.ddns.net/api/history/' + count);
 }
