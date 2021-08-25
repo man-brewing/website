79fa7c3f36ba88daab6ae6747abe8645a88e35ff
Nathan Dykstra <dykstra.nathan@gmail.com>
2021-08-24 22:39:02 -0500

    add cookies for age verification
    closes #17

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import rootReducer from './reducers/rootReducer'
 import middleware from './middleware';
+import { CookiesProvider } from 'react-cookie';
 
 const store = createStore(rootReducer, middleware)
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><CookiesProvider><App /></CookiesProvider></Provider>, document.getElementById('root'));
