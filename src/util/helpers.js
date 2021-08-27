8f3293fcc4331e5032539dbc2e4e74c8a9d1c88f
Nathan Dykstra <dykstra.nathan@gmail.com>
2021-08-26 21:03:27 -0500

    add F to C temperature convert function

--- src/util/helpers.js
+++ src/util/helpers.js
@@ -1,3 +1,19 @@
+/**
+ * Gets the index of the first item that has the matching attribute.
+ * @param {*} array 
+ * @param {*} attr 
+ * @param {*} value 
+ * @returns 
+ */
 export function indexOfByAttr(array, attr, value) {
     return array.findIndex(item => item[attr] === value);
+}
+
+/**
+ * Converts a Celsius measurement to Fahrenheit.
+ * @param {*} celsius 
+ * @returns 
+ */
+export function toFahrenheit(celsius) {
+    return (celsius * 1.8) + 32;
 }
